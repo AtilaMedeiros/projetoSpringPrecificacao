01510cdab30b9c357831bd301a68b6bb92f65365
AtilaMedeiros <atila.medeiros@gmail.com>
2022-09-06 10:55:57 -0300

    instalando actuator e spring boot admin

---
+++ springAdmin/src/test/java/com/lab/springAdmin/SpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.springAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
